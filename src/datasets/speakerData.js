471470c341a16048a5c6746913fab173d3d4f757
danmkang <danminkang@gmail.com>
2019-01-03 12:30:38 -0500

    Reset to start

---
+++ src/datasets/speakerData.js
@@ -0,0 +1,19 @@
+import speaker from '../assets/speaker.png';
+import speakerAvrum from '../assets/avrum.jpeg';
+
+const speakerData = [
+    {
+        img: `${speakerAvrum}`,
+        name: 'Avrum Laurie',
+        title: 'Head of Product',
+        company: 'Wealthsimple'
+    },
+    {
+        img: `${speaker}`,
+        name: '',
+        title: 'TBD',
+        company: ''
+    },
+]
+
+export default speakerData;
