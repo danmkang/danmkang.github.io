8496274f1e0b57e1d5ce7700cd93e46be815b31d
danmkang <danminkang@gmail.com>
2019-01-08 23:36:52 -0500

    Updates

---
+++ precache-manifest.40bae3ae88e04188b415d39995369d2b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "c57d5f893c07523f4f12",
+    "url": "/static/css/main.d394ebb4.chunk.css"
+  },
+  {
+    "revision": "c57d5f893c07523f4f12",
+    "url": "/static/js/main.c57d5f89.chunk.js"
+  },
+  {
+    "revision": "44628870f93ae4361b5e",
+    "url": "/static/js/1.44628870.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c3e86c3193df3b5a89f1df6df8a57275",
+    "url": "/static/media/hero-gradient-cut.c3e86c31.png"
+  },
+  {
+    "revision": "700be3ca90519516696480e9493d0ab0",
+    "url": "/static/media/header-logo.700be3ca.png"
+  },
+  {
+    "revision": "1a62b0b5d43b6f37cf1a9e227e7533e0",
+    "url": "/static/media/avrum.1a62b0b5.jpeg"
+  },
+  {
+    "revision": "3560b2ae7914fe74f851886531942592",
+    "url": "/static/media/speaker-card.3560b2ae.png"
+  },
+  {
+    "revision": "15f7be7334f4f51045021f2d8879b6c1",
+    "url": "/static/media/partner-card.15f7be73.png"
+  },
+  {
+    "revision": "aa2bd7a77ab7076e6c9a510235be4258",
+    "url": "/static/media/host-card.aa2bd7a7.png"
+  },
+  {
+    "revision": "e342e1bbbfd3a3b80ce2642f7d75e226",
+    "url": "/static/media/footer-logo.e342e1bb.png"
+  },
+  {
+    "revision": "c55d06cf2744eb718b2f73ecf4627fbe",
+    "url": "/index.html"
+  }
+];
